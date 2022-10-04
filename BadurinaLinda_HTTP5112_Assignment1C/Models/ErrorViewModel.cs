ae8b370beac1f3abc091ae6c9e56b1bdaba22df7
Linda Badurina <lindabadurina@hotmail.com>
2022-10-04 01:18:06 -0400

    Initial commit

---
+++ BadurinaLinda_HTTP5112_Assignment1C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BadurinaLinda_HTTP5112_Assignment1C.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
